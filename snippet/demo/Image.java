ee47418b78a15b70d0c6139d0d20c2d4aef086f8
litefy <litefy@gmail.com>
2013-03-13 17:30:16 +0800

    nil

---
+++ snippet/demo/Image.java
@@ -0,0 +1,53 @@
+/**  
+* 项目名：系统项目名称
+  
+* 包名：snippet.demo
+  
+* 文件名：Image.java
+  
+* 版本信息：
+  
+* 日期：2013-3-13-下午5:29:49
+  
+* Copyright (c) 2013百分点公司-版权所有
+  
+*   
+*/
+package snippet.demo;
+
+/**  
+ *   
+ * 类名称：Image
+  
+ * 类描述：
+  
+ * 创建人：chao.fang
+  
+ * 修改人：chao.fang
+  
+ * 修改时间：2013-3-13 下午5:29:49
+  
+ * 修改备注：
+  
+ * @version 1.0.0
+  
+ *   
+ */
+public class Image {
+
+	/** 
+	 * main 这里用一句话描述这个方法的作用
+	
+	 * @condition optional
+	 
+	 * @param args void 
+	
+	 * @exception 
+	 * @since  1.0.0 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
